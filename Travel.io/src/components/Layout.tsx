714fcac6a502e98309df1c339ac3a1af28b06c1b
satyam969 <satyamtiwari87090@gmail.com>
2025-07-09 12:49:23 +0530

    meta tags

--- Travel.io/src/components/Layout.tsx
+++ Travel.io/src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import  { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Sidebar from './Sidebar';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { checkAuth } from '../utils/verifytoken';
